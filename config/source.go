523715c794ebe9a9514d62af66ca252cf1983896
oooopera <sweetbox-sx@qq.com>
2023-01-17 10:40:29 +0800

    2

--- config/source.go
+++ config/source.go
@@ -1,6 +1,6 @@
 package config
 
-import "github.com/ooooepra/proxypool/pkg/tool"
+import "github.com/oooopera/proxypool/pkg/tool"
 
 type Source struct {
 	Type    string       `json:"type" yaml:"type"`
